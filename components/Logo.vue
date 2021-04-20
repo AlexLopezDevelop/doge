127e0c3aebf312e1d2f929f672bd86b0215b1b83
Alex López <32960226+AlexLopezDevelop@users.noreply.github.com>
2021-04-20 17:28:09 +0200

    Update Logo.vue

--- components/Logo.vue
+++ components/Logo.vue
@@ -5,4 +5,24 @@
 </template>
 
 <style>
+  @media only screen and (min-width: 600px) {
+        .centerdoge {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 35%;
+          animation: float 3s ease-in-out infinite;
+        }
+  }
+
+  @media only screen and (max-width: 600px) {
+    .centerdoge {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+      animation: float 6s ease-in-out infinite;
+    
+    }
+  }
 </style>
